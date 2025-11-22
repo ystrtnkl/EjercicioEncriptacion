f044604e0e53270a154cd3148ccf36f258a4ca76
RackGamesDev <yesterdiscord7@gmail.com>
2025-11-22 14:04:28 +0100

    build linux

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,8 @@
 FROM python:3.11-slim
 WORKDIR /app
+RUN apt-get update && \
+    apt-get install -y --no-install-recommends binutils && \
+    pip install --no-cache-dir -r requirements.txt || true
 COPY requirements.txt /app/
 RUN pip install --no-cache-dir -r requirements.txt || true
 CMD ["python3", "encriptacion.py"]
--- probar.sh
+++ probar.sh
@@ -8,3 +8,9 @@ sudo chown root:root claves.bin
 sudo chown root:root admin-privada.pem
 sudo chown root:root admin-publica.pem
 sudo chown root:root admin.key
+#build en linux
+docker run -it --rm -v "$(pwd)":/app -u $(id -u):$(id -g) entorno-python sh -c "cd /app && pyinstaller --onefile --clean --noconfirm encriptacion.py"
+#build en windows
+docker pull mcr.microsoft.com/powershell
+docker run -it --rm -v /your/project/path:/app mcr.microsoft.com/powershell cmd /S /C "cd /app && pyinstaller --onefile --clean --noconfirm your_script.py"
+
