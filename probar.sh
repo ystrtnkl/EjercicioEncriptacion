546a1992a2d2565cc838f7b79d4e37bb95915cab
RackGamesDev <yesterdiscord7@gmail.com>
2025-11-22 15:15:59 +0100

    build windows

--- probar.sh
+++ probar.sh
@@ -10,7 +10,7 @@ sudo chown root:root admin-publica.pem
 sudo chown root:root admin.key
 #build en linux
 docker run -it --rm -v "$(pwd)":/app -u $(id -u):$(id -g) entorno-python sh -c "cd /app && pyinstaller --onefile --clean --noconfirm encriptacion.py"
-#build en windows
-docker pull mcr.microsoft.com/powershell
-docker run -it --rm -v /your/project/path:/app mcr.microsoft.com/powershell cmd /S /C "cd /app && pyinstaller --onefile --clean --noconfirm your_script.py"
+#build en windows (ejecutar en powershell en una maquina virtual de windows con python3 y pip installados)
+pip install pyinstaller
+pyinstaller --onefile --clean --noconfirm encriptacion.py
 
